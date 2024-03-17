7a351e2828717fd28b0ab0547a2bd82f0b414b45
unitehenry <unitehenry@gmail.com>
2024-03-17 15:01:56 -0700

    overwrite output file

--- VMTranslator.py
+++ VMTranslator.py
@@ -11,13 +11,13 @@ def get_output_path(filename):
     return '/'.join(path)
 
 def main():
-    file = open(sys.argv[1], 'r')
-    filename = get_filename()
-    tokens = parse_tokens(file.read())
-    instructions = translate(tokens, filename)
+    instuctions = []
+    with open(sys.argv[1], 'r') as input_file:
+        filename = get_filename()
+        tokens = parse_tokens(input_file.read())
+        instructions = translate(tokens, filename)
     output_path = get_output_path(filename)
-    output = open(output_path, 'a')
-    output.write('\n'.join(instructions))
-    output.close()
-
+    with open(output_path, 'w') as output_file:
+        output_file.write('\n'.join(instructions))
+        output_file.close()
 main()
