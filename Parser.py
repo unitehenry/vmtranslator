73bae0f11039068947285c843d5a32a5a60050fd
unitehenry <unitehenry@gmail.com>
2024-03-17 14:34:42 -0700

    parse inline comments

--- Parser.py
+++ Parser.py
@@ -4,5 +4,7 @@ def parse_tokens(content):
         line_string = line.strip()
         if not line_string: continue
         if '//' in line_string and line_string.index('//') == 0: continue
+        if '//' in line_string and line_string.index('//') != 0:
+            line_string = line.split('//')[0].strip()
         tokens.append(line_string)
     return tokens
